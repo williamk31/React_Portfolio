0f3520e0ad92277ad607f08eb67ffeb06422bd2e
goofy foot <williamk31@gmail.com>
2024-05-10 14:57:25 -0600

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <nav className="navbar fixed-bottom bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="https://www.github.com/williamk31">
+                    <img src="/icons8-lobster-24.png"></img>
+                </a>
+            </div>
+        </nav>
+    )
+}
